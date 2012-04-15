acc5227eab510e2c29e1c55d8d2def147a5245d8
rf <rf0444@gmail.com>
2012-04-15 17:36:11 +0900

    Yesod 1.0 対応

--- Foundation.hs
+++ Foundation.hs
@@ -1,8 +1,8 @@
 module Foundation
-    ( Hoge (..)
+    ( App (..)
     , Route (..)
-    , HogeMessage (..)
-    , resourcesHoge
+    , AppMessage (..)
+    , resourcesApp
     , Handler
     , Widget
     , Form
@@ -15,7 +15,6 @@ module Foundation
 import Prelude
 import Yesod
 import Yesod.Static
-import Settings.StaticFiles
 import Yesod.Auth
 import Yesod.Auth.BrowserId
 import Yesod.Auth.GoogleEmail
@@ -23,29 +22,21 @@ import Yesod.Default.Config
 import Yesod.Default.Util (addStaticContentExternal)
 import Yesod.Logger (Logger, logMsg, formatLogText)
 import Network.HTTP.Conduit (Manager)
-#ifdef DEVELOPMENT
-import Yesod.Logger (logLazyText)
-#endif
 import qualified Settings
-import qualified Data.ByteString.Lazy as L
 import qualified Database.Persist.Store
-import Database.Persist.GenericSql
+import Settings.StaticFiles
+import Database.Persist.MongoDB
 import Settings (widgetFile, Extra (..))
 import Model
 import Text.Jasmine (minifym)
 import Web.ClientSession (getKey)
 import Text.Hamlet (hamletFile)
-#if DEVELOPMENT
-import qualified Data.Text.Lazy.Encoding
-#else
-import Network.Mail.Mime (sendmail)
-#endif
 
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
 -- access to the data present here.
-data Hoge = Hoge
+data App = App
     { settings :: AppConfig DefaultEnv Extra
     , getLogger :: Logger
     , getStatic :: Static -- ^ Settings for static file serving.
@@ -55,7 +46,7 @@ data Hoge = Hoge
     }
 
 -- Set up i18n messages. See the message folder.
-mkMessage "Hoge" "messages" "en"
+mkMessage "App" "messages" "en"
 
 -- This is where we define all of the routes in our application. For a full
 -- explanation of the syntax, please see:
@@ -63,30 +54,33 @@ mkMessage "Hoge" "messages" "en"
 --
 -- This function does three things:
 --
--- * Creates the route datatype HogeRoute. Every valid URL in your
+-- * Creates the route datatype AppRoute. Every valid URL in your
 --   application can be represented as a value of this type.
 -- * Creates the associated type:
---       type instance Route Hoge = HogeRoute
--- * Creates the value resourcesHoge which contains information on the
+--       type instance Route App = AppRoute
+-- * Creates the value resourcesApp which contains information on the
 --   resources declared below. This is used in Handler.hs by the call to
 --   mkYesodDispatch
 --
 -- What this function does *not* do is create a YesodSite instance for
--- Hoge. Creating that instance requires all of the handler functions
+-- App. Creating that instance requires all of the handler functions
 -- for our application to be in scope. However, the handler functions
--- usually require access to the HogeRoute datatype. Therefore, we
+-- usually require access to the AppRoute datatype. Therefore, we
 -- split these actions into two functions and place them in separate files.
-mkYesodData "Hoge" $(parseRoutesFile "config/routes")
+mkYesodData "App" $(parseRoutesFile "config/routes")
 
-type Form x = Html -> MForm Hoge Hoge (FormResult x, Widget)
+type Form x = Html -> MForm App App (FormResult x, Widget)
 
 -- Please see the documentation for the Yesod typeclass. There are a number
 -- of settings which can be configured by overriding methods here.
-instance Yesod Hoge where
+instance Yesod App where
     approot = ApprootMaster $ appRoot . settings
 
-    -- Place the session key file in the config folder
-    encryptKey _ = fmap Just $ getKey "config/client_session_key.aes"
+    -- Store session data on the client in encrypted cookies,
+    -- default session idle timeout is 120 minutes
+    makeSessionBackend _ = do
+        key <- getKey "config/client_session_key.aes"
+        return . Just $ clientSessionBackend key 120
 
     defaultLayout widget = do
         master <- getYesod
@@ -100,6 +94,7 @@ instance Yesod Hoge where
 
         pc <- widgetToPageContent $ do
             $(widgetFile "normalize")
+            addStylesheet $ StaticR css_bootstrap_css
             $(widgetFile "default-layout")
         hamletToRepHtml $(hamletFile "templates/default-layout-wrapper.hamlet")
 
@@ -121,12 +116,12 @@ instance Yesod Hoge where
     -- users receiving stale content.
     addStaticContent = addStaticContentExternal minifym base64md5 Settings.staticDir (StaticR . flip StaticRoute [])
 
-    -- Enable Javascript async loading
-    yepnopeJs _ = Just $ Right $ StaticR js_modernizr_js
+    -- Place Javascript at bottom of the body tag so the rest of the page loads first
+    jsLoader _ = BottomOfBody
 
 -- How to run database actions.
-instance YesodPersist Hoge where
-    type YesodPersistBackend Hoge = SqlPersist
+instance YesodPersist App where
+    type YesodPersistBackend App = Action
     runDB f = do
         master <- getYesod
         Database.Persist.Store.runPool
@@ -134,13 +129,13 @@ instance YesodPersist Hoge where
             f
             (connPool master)
 
-instance YesodAuth Hoge where
-    type AuthId Hoge = UserId
+instance YesodAuth App where
+    type AuthId App = UserId
 
     -- Where to send a user after successful login
-    loginDest _ = RootR
+    loginDest _ = HomeR
     -- Where to send a user after logout
-    logoutDest _ = RootR
+    logoutDest _ = HomeR
 
     getAuthId creds = runDB $ do
         x <- getBy $ UniqueUser $ credsIdent creds
@@ -154,15 +149,14 @@ instance YesodAuth Hoge where
 
     authHttpManager = httpManager
 
--- Sends off your mail. Requires sendmail in production!
-deliver :: Hoge -> L.ByteString -> IO ()
-#ifdef DEVELOPMENT
-deliver y = logLazyText (getLogger y) . Data.Text.Lazy.Encoding.decodeUtf8
-#else
-deliver _ = sendmail
-#endif
-
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
-instance RenderMessage Hoge FormMessage where
+instance RenderMessage App FormMessage where
     renderMessage _ _ = defaultFormMessage
+
+-- Note: previous versions of the scaffolding included a deliver function to
+-- send emails. Unfortunately, there are too many different options for us to
+-- give a reasonable default. Instead, the information is available on the
+-- wiki:
+--
+-- https://github.com/yesodweb/yesod/wiki/Sending-email
